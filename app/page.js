3b096c86fe55b66cdd2c524f7c7ce5335c746073
Ahmed B. <ahmedbashaar974@gmail.com>
2024-08-13 17:54:26 +0500

    a
    a

--- app/page.js
+++ app/page.js
@@ -3,16 +3,8 @@ import Background from "./components/background";
 
 export default function Home() {
   return (
-    <div style={{ position: 'relative', height: '100vh', width: '100vw' }}>
-      <Main />
-      <div style={{ position: 'absolute', top: 0, left: 0, height: '100%', width: '100%', zIndex: 0 }}>
-        <script
-          type="module"
-          src="https://unpkg.com/@splinetool/viewer@1.9.6/build/spline-viewer.js"
-          async
-        ></script>
-        <spline-viewer url="https://prod.spline.design/AF5ZqcFihO7h630V/scene.splinecode"></spline-viewer>
-      </div>
+    <div>
+      <Background />
     </div>
   );
 }
