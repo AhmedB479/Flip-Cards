4b4e83f98825768134b723e4a7832f54f0acdf69
Ahmed B. <ahmedbashaar974@gmail.com>
2024-08-13 23:13:53 +0500

    b
    b

--- app/page.js
+++ app/page.js
@@ -5,8 +5,23 @@ const Background = dynamic(() => import('./components/background'),{ssr:false})
 
 export default function Home() {
   return (
-    <div>
-      <Background/>
+    <div style={{ position: 'relative', zIndex: '1' }}>
+      <Background />
+      <button style={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        padding: '10px 20px',
+        backgroundColor: '#ED9121',
+        color: '#fff',
+        border: 'none',
+        borderRadius: '5px',
+        cursor: 'pointer',
+        zIndex: '2' // Ensure the button is on top of the background
+      }}>
+        Click Me
+      </button>
     </div>
   );
 }
