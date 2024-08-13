70ad10b19bdcdda6de62f8abff12bde85a3027d7
Ahmed B. <ahmedbashaar974@gmail.com>
2024-08-13 17:14:01 +0500

    a
    a

--- app/page.js
+++ app/page.js
@@ -2,8 +2,12 @@ import Image from "next/image";
 import Background from "./components/background";
 export default function Home() {
   return (
-    <div>
-<script type="module" src="https://unpkg.com/@splinetool/viewer@1.9.6/build/spline-viewer.js"></script>
-<spline-viewer url="https://prod.spline.design/AF5ZqcFihO7h630V/scene.splinecode"></spline-viewer>    </div>
+    <body>
+    <div style={{zIndex:'0',position:'fixed'}}>
+      <script type="module" src="https://unpkg.com/@splinetool/viewer@1.9.6/build/spline-viewer.js"></script>
+      <spline-viewer url="https://prod.spline.design/AF5ZqcFihO7h630V/scene.splinecode"></spline-viewer>
+    </div>
+      <h1 style={{zIndex:'1'}}>hello world</h1>
+    </body>
   );
 }
