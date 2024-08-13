44be1f20116d59579b3adb714c5e4bf20f52c60f
Ahmed B. <ahmedbashaar974@gmail.com>
2024-08-13 17:04:03 +0500

    test
    test

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Background from "./components/background";
 export default function Home() {
   return (
     <div>
-      <Background />
-    </div>
+<script type="module" src="https://unpkg.com/@splinetool/viewer@1.9.6/build/spline-viewer.js"></script>
+<spline-viewer url="https://prod.spline.design/AF5ZqcFihO7h630V/scene.splinecode"></spline-viewer>    </div>
   );
 }
